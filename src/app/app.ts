de4d184fd888bda5eafdb0aeaec0ddc27bf993ed
solidados <geekbrains.pk@internet.ru>
2026-04-29 16:18:22 +0400

    chore: wire up `app.component` with layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BreadcrumbsComponent } from '@shared/ui/breadcrumbs/breadcrumbs.component/breadcrumbs.component';
+import { FooterComponent } from '@shared/ui/footer/footer.component/footer.component';
+import { HeaderComponent } from '@shared/ui/header/header.component/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, BreadcrumbsComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('film-collection');
