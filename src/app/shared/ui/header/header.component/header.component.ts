fc2eef969803b5a11194d7f52cc054c8b75a558d
solidados <geekbrains.pk@internet.ru>
2026-05-01 15:31:27 +0400

    feat: add burger button and mobile menu

--- src/app/shared/ui/header/header.component/header.component.ts
+++ src/app/shared/ui/header/header.component/header.component.ts
@@ -1,6 +1,6 @@
-import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { DOCUMENT, NgOptimizedImage } from '@angular/common';
+import { Component, effect, ElementRef, HostListener, inject, signal } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,4 +9,47 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isMenuOpen = signal<boolean>(false);
+  private readonly el = inject(ElementRef<HTMLElement>);
+  private readonly document = inject(DOCUMENT);
+
+  constructor() {
+    effect((onCleanup) => {
+      const isOpen = this.isMenuOpen();
+      const isMobile = window.innerWidth <= 768;
+
+      if (isOpen && isMobile) {
+        this.document.body.classList.add('no-scroll');
+
+        onCleanup(() => {
+          this.document.body.classList.remove('no-scroll');
+        });
+      }
+    });
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen.update((curr) => !curr);
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen.set(false);
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(e: MouseEvent): void {
+    const target = e.target as Node;
+
+    if (this.isMenuOpen() && !this.el.nativeElement.contains(target)) {
+      this.closeMenu();
+    }
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    if (window.innerWidth > 768 && this.isMenuOpen()) {
+      this.closeMenu();
+    }
+  }
+}
